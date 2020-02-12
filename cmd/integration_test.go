c3d805b689f125c4240b99e7a8b2b6bf0ba5e630
Ayan George <ayan@ayan.net>
2020-02-12 11:29:41 -0500

    refactor(tests): Update tests to use new API

--- cmd/integration_test.go
+++ cmd/integration_test.go
@@ -13,6 +13,7 @@ import (
 
 type TestRunCommand struct {
 	*run.Command
+	cancel context.CancelFunc
 
 	// Temporary directory used for default data, meta, and wal dirs.
 	Dir string
@@ -59,8 +60,8 @@ func NewTestRunCommand(env map[string]string) *TestRunCommand {
 }
 
 // MustRun calls Command.Run and panics if there is an error.
-func (c *TestRunCommand) MustRun() {
-	if err := c.Command.Run(context.Background(), "-config", os.DevNull); err != nil {
+func (c *TestRunCommand) MustRun(ctx context.Context) {
+	if err := c.Command.Run(ctx, "-config", os.DevNull); err != nil {
 		panic(err)
 	}
 }
@@ -87,6 +88,5 @@ func (c *TestRunCommand) BoundHTTPAddr() string {
 }
 
 func (c *TestRunCommand) Cleanup() {
-	c.Command.Close()
 	os.RemoveAll(c.Dir)
 }
