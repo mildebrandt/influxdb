bf2281d018344331346dcfa640f7555fb2f3e241
Ayan George <ayan@ayan.net>
2020-02-12 11:23:39 -0500

    refactor(tests): Have test account for new error
    This commit adds a check for the "Client.Timeout exceeded" error
    message.
    
    The error format

--- client/influxdb_test.go
+++ client/influxdb_test.go
@@ -744,10 +744,13 @@ func TestClient_Timeout(t *testing.T) {
 	}
 	query := client.Query{}
 	_, err = c.Query(query)
+
+	t.Logf("c.Query() error is %v", err)
 	if err == nil {
 		t.Fatalf("unexpected success. expected timeout error")
 	} else if !strings.Contains(err.Error(), "request canceled") &&
-		!strings.Contains(err.Error(), "use of closed network connection") {
+		!strings.Contains(err.Error(), "use of closed network connection") &&
+		!strings.Contains(err.Error(), "Client.Timeout exceeded") {
 		t.Fatalf("unexpected error. expected 'request canceled' error, got %v", err)
 	}
 }
